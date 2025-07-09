1d2cd8d4691cbd79062b05d06312836e2339dabd
sara-lai <shihualai@gmail.com>
2025-07-09 19:15:36 +0800

    bring currentUser into dashboard; corner profile pic menu

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from 'react'
 import { Flex, Box, Text  } from '@chakra-ui/react';
-import SignOut from "./SignOut"
+import * as userService from '../../services/userService'
+import { useAuth } from '@clerk/clerk-react'
 import './dashboard.css'
 import '../Onboarding/onboarding.css'
+import ProfilePicMenu from './ProfilePicMenu'
 
 const Dashboard = () => {
 
+     const { getToken } = useAuth()
+
+     const [currentUser, setCurrentUser] = useState({})
+
+    async function loadCurrentUser() {
+        const token = await getToken()
+        const user = await userService.getCurrentUser(token)
+        console.log('the current user on dashboard', user)
+        setCurrentUser(user.user)
+    }
+
+    // load currentUser
+    useEffect( () => {
+        loadCurrentUser()
+    }, [] )
+
     return (
         <div className='dashboard-container'> 
-            <div className='upper-right-profile-misc'>
-                <span>Edit profile</span>
-                <SignOut />                
+            <div className='upper-right-profile-misc'>               
+                <ProfilePicMenu userInfo={currentUser} />  
             </div>
             <Flex maxW="1600px" mx="auto" minH="80vh" gap={4}>
                 <Box flex="0 0 20%" bg="white" p={4} borderRadius="md" boxShadow="md">
@@ -35,10 +53,6 @@ const Dashboard = () => {
                         <Box className="content-feed" flex="0 0 70%"  bg="white" p={4}  borderRadius="md" boxShadow="md">
                              <Text>Search Bar</Text>      
                             <img style={{ maxHeight: '80px'}} src='/images/nd-search-bar.png' />
-                             <Text>New Post +</Text>        
-                            <img style={{ maxHeight: '80px'}} src='/images/nd-new-post.png' />  
-                            <Text>Content Feed</Text>                                                                
-                            <img src='/images/nd-content1.png' />
                         </Box>
                         <Box className="side-content" flex="0 0 30%"  bg="white" borderRadius="md" boxShadow="md"  p={4}>
                             <Text>Misc Side panel</Text>
---
+++ src/components/Dashboard/ProfilePicMenu.jsx
@@ -0,0 +1,30 @@
+import { Menu, MenuButton, MenuList, MenuItem, Flex, Avatar, Icon } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons'
+
+import SignOut from "./SignOut"
+
+const ProfilePicMenu = ({ userInfo }) => {
+
+    console.log('what is the userInfo', userInfo)
+    return (
+        <Menu>
+            <MenuButton>
+                <Flex align="center" gap={1}>
+                    <Avatar size="sm" src={userInfo.profileImg} name={userInfo.fullName || 'User'} />
+                    <Icon as={ChevronDownIcon} w={4} h={4} />
+                </Flex>
+            </MenuButton>
+            <MenuList>
+                 <Flex direction="column" align="center" gap={2}>
+                    <Avatar size="md" src={userInfo.profileImg} name={userInfo.fullName || 'User'} />
+                    <p>{userInfo.fullName}</p>
+                    <p>{userInfo.neighbourhood}</p>
+                </Flex>
+                <MenuItem>View Profile</MenuItem>
+                <MenuItem> <SignOut /></MenuItem>
+            </MenuList>
+        </Menu>        
+    )
+}
+
+export default ProfilePicMenu

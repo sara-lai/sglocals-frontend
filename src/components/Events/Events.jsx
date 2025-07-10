fb5c630f41660e65833191616384a663d68e3892
yuukka <yuukinba0105@gmail.com>
2025-07-10 21:47:18 +0800

    CSS Update

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,12 +1,44 @@
 import { useState, useContext, useEffect } from "react";
 import EventCard from "./EventCard";
+import { Flex, Box, Text, Grid  } from '@chakra-ui/react';
+import './Events.css'
 
 const Events = (props) => {
+const cards = 6;
+const eventCards= [];
+for (let i=0; i<cards; i++) {
+   eventCards.push(<EventCard />);
+}
+
 
     return (
         <>
-            <h1>Event List</h1>
-            <EventCard/>
+        <div className='flex'> 
+            <Box bg="white" p={4} borderRadius="md" boxShadow="md" className='sidebar' >
+                <div className='side-navbar'>
+                    <div className='logo-side-nav'>
+                        <img src='/images/flowers1.png' />
+                        <span>Kampong Lah</span>              
+                    </div>    
+                    <div className='side-links'>
+                        <p>Home</p>
+                        <p>I Buy U Buy</p>
+                        <p>Groups</p>
+                        <p>Events</p>
+                        <p>Chats</p>
+                        <p>+ Post</p>
+                    </div>
+                    <Text>E.g.:</Text>
+                    {/* <img  style={{ width: '200px'}} src='/images/nd-sidebar.png' /> */}
+                </div>
+            </Box>
+            <Grid templateColumns="repeat(2, 1fr)" gap="6">
+
+                {eventCards}
+            </Grid>
+        </div>
+
+
         </>
 
     );

eb6d6242721560b95226d579aaa6b0da53a47845
sara-lai <shihualai@gmail.com>
2025-07-13 20:16:58 +0800

    Layout with currentUser for use in dashboard and other pages

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -43,47 +43,20 @@ const Dashboard = () => {
 
     return (
         <div className='dashboard-container'> 
-            <div className='upper-right-profile-misc'>               
-                <ProfilePicMenu userInfo={currentUser} />  
-            </div>
-            <Flex maxW="1600px" mx="auto" minH="80vh" gap={4}>
-                <Box flex="0 0 20%" bg="white" p={4} borderRadius="md" boxShadow="md">
-                <div className='side-navbar'>
-                    <div className='logo-side-nav'>
-                        <img src='/images/flowers1.png' />
-                        <span>Kampong Lah</span>              
-                    </div>    
-                    <div className='side-links'>
-                        <p>Home</p>
-                        <p>I Buy U Buy</p>
-                        <p>Groups</p>
-                        <p>Events</p>
-                        <p>Chats</p>
-                        <p>+ Post</p>
-                    </div>
-                    <Text>E.g.:</Text>
-                    <img  style={{ width: '200px'}} src='/images/nd-sidebar.png' />
-                </div>
+            <Flex maxW="1000px"  minH="80vh" gap={4}>
+                <Box flex="0 0 70%"  bg="white" p={4}  borderRadius="md" > 
+                    <img style={{ maxHeight: '80px'}} src='/images/nd-search-bar.png' />
+
+                    <NewPost userInfo={currentUser} addTopOfFeed={addTopOfFeed} />
+
+                    <ContentFeed theFeed={contentFeed} />
+
                 </Box>
-                <Box flex="0 0 80%" bg="white" p={4} >
-                    <Flex maxW="1000px"  minH="80vh" gap={4}>
-                        <Box flex="0 0 70%"  bg="white" p={4}  borderRadius="md" > 
-                            <img style={{ maxHeight: '80px'}} src='/images/nd-search-bar.png' />
-
-                            <NewPost userInfo={currentUser} addTopOfFeed={addTopOfFeed} />
-
-                            <ContentFeed theFeed={contentFeed} />
-
-                        </Box>
-                        <Box className="side-content" flex="0 0 30%"  bg="white" p={4} borderRadius="md" boxShadow="md">
-                            <Text>Misc Side panel</Text>
-                            <img src='/images/nd-content2.png' />
-                        </Box>
-                    </Flex>
+                <Box className="side-content" flex="0 0 30%"  bg="white" p={4} borderRadius="md" boxShadow="md">
+                    <Text>Misc Side panel</Text>
+                    <img src='/images/nd-content2.png' />
                 </Box>
-            </Flex>
-
-
+            </Flex> 
         </div>
     )
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import { Flex, Box, Text  } from '@chakra-ui/react';
+import { Outlet } from "react-router"
+import { useAuth } from '@clerk/clerk-react'
+
+import './Dashboard/dashboard.css' // temporary
+
+import ProfilePicMenu from './Dashboard/ProfilePicMenu'
+import * as userService from '../services/userService'
+
+
+const Layout = () => {
+
+    const { getToken } = useAuth()
+    const [currentUser, setCurrentUser] = useState({})
+
+    async function loadCurrentUser(){
+        const token = await getToken()
+        const user = await userService.getCurrentUser(token)
+        setCurrentUser(user)
+    }
+
+    useEffect( () => {
+        loadCurrentUser()
+    }, [] )
+
+    return (
+        <div className='app-wrapper'>
+            <div className='upper-right-profile-misc'>               
+                <ProfilePicMenu userInfo={currentUser} />  
+            </div>
+            <Flex maxW="1600px" mx="auto" minH="80vh" gap={4}>
+                <Box flex="0 0 20%" bg="white" p={4} borderRadius="md" boxShadow="md">
+                    <div className='side-navbar'>
+                        <div className='logo-side-nav'>
+                            <img src='/images/flowers1.png' />
+                            <span>Kampong Lah</span>              
+                        </div>    
+                        <div className='side-links'>
+                            <p>Home</p>
+                            <p>I Buy U Buy</p>
+                            <p>Groups</p>
+                            <p>Events</p>
+                            <p>Chats</p>
+                            <p>+ Post</p>
+                        </div>
+                        <Text>E.g.:</Text>
+                        <img  style={{ width: '200px'}} src='/images/nd-sidebar.png' />
+                    </div>
+                </Box>
+                <Box flex="0 0 80%" bg="white" p={4} >
+                    <Outlet />
+                </Box>
+            </Flex>
+        </div>
+
+
+    )
+}
+
+export default Layout
